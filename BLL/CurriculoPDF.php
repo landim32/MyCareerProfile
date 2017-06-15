31334a5717e10a90bdbc5d4626a793d6742bc9aa
Rodrigo <rodrigo@emagine.com.br>
2017-06-15 14:57:12 -0300

    .

--- BLL/CurriculoPDF.php
+++ BLL/CurriculoPDF.php
@@ -65,12 +65,12 @@ class CurriculoPDF extends FPDF
         $this->SetTextColor(120, 120, 120);
         $this->Cell(0,5,$curriculo->getCargoAtual(), 0, 1);
 
-        $this->Line(10, $this->GetY(), 160, $this->GetY());
+        $this->Line(10, $this->GetY(), $this->GetPageWidth(), $this->GetY());
         $this->SetXY($this->GetX(), $this->GetY() + 5);
 
         $this->SetFont('Arial','',9);
         $this->SetTextColor(0, 0, 0);
-        $this->Cell(0,5,"Telefone: ");
+        $this->Cell(30,5,"Telefone: ");
         $this->Cell(0,5,$curriculo->getTelefone1(), 0, 1);
     }
 
