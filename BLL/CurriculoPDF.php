c6dc01684e1198784f82f57d0c092f060c29c6cd
Rodrigo <rodrigo@emagine.com.br>
2017-06-15 14:55:31 -0300

    .

--- BLL/CurriculoPDF.php
+++ BLL/CurriculoPDF.php
@@ -59,11 +59,19 @@ class CurriculoPDF extends FPDF
     private function gerarDados($curriculo) {
         $this->SetFont('Arial','B',16);
         $this->SetTextColor(0, 0, 0);
-        $this->Cell(0,5,$curriculo->getNome(), 0 ,1);
+        $this->Cell(0,6,$curriculo->getNome(), 0 ,1);
 
         $this->SetFont('Arial','',10);
         $this->SetTextColor(120, 120, 120);
         $this->Cell(0,5,$curriculo->getCargoAtual(), 0, 1);
+
+        $this->Line(10, $this->GetY(), 160, $this->GetY());
+        $this->SetXY($this->GetX(), $this->GetY() + 5);
+
+        $this->SetFont('Arial','',9);
+        $this->SetTextColor(0, 0, 0);
+        $this->Cell(0,5,"Telefone: ");
+        $this->Cell(0,5,$curriculo->getTelefone1(), 0, 1);
     }
 
     public function gerar() {
