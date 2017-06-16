2221e529079a301ab248f80a1e4b0a662b16110c
Rodrigo <rodrigo@emagine.com.br>
2017-06-16 11:16:53 -0300

    .

--- Core/config.inc.php
+++ Core/config.inc.php
@@ -2,7 +2,7 @@
 
 ini_set('memory_limit', '120M');
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+//setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 
 define("PROFILE", $_GET["profile"]);
