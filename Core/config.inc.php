5f586de5674ab5a66bc658b46b20e51999e1e4a6
Rodrigo <rodrigo@emagine.com.br>
2019-01-25 22:28:07 -0200

    Atualizando

--- Core/config.inc.php
+++ Core/config.inc.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 //setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 
-define("PROFILE", $_GET["profile"]);
-//define("PROFILE", "rodrigo");
-//define("BASE_PATH", "/rodrigo");
-define("BASE_PATH", "/MyCareerProfile");
+define("PROFILE", "rodrigo");
+define("BASE_PATH", "/rodrigo");
+//define("PROFILE", $_GET["profile"]);
+//define("BASE_PATH", "/MyCareerProfile");
 define("TEMA_PATH", BASE_PATH . "/theme");
